086c77de5f33e816e596979ad2f75658771e275e
praveenaaa <48066696+praveenaaa@users.noreply.github.com>
2020-07-10 14:34:15 +0530

    Update Test_steps.java
    changed driver

--- src/test/java/step_definition/Test_steps.java
+++ src/test/java/step_definition/Test_steps.java
@@ -16,7 +16,7 @@ public class Test_steps {
 	static WebDriver driver;
 	@Given("User is on Home Page")
 	public void user_is_on_Home_Page() {
-		System.setProperty("webdriver.chrome.driver", "C://driver9999//chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\driver_cucumber\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
